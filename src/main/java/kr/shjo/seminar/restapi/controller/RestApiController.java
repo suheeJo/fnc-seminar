d09033b14147523fb348d5e1bce26b91ebaf5253
suheeJo <jo9172sh@gmail.com>
2016-01-13 11:05:35 +0900

    REST API 테스트 컨트롤러 추가

--- src/main/java/kr/shjo/seminar/restapi/controller/RestApiController.java
+++ src/main/java/kr/shjo/seminar/restapi/controller/RestApiController.java
@@ -1,5 +1,6 @@
 package kr.shjo.seminar.restapi.controller;
 
+import java.util.Arrays;
 import java.util.List;
 
 import javax.validation.Valid;
@@ -21,6 +22,8 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 public class RestApiController {
 	
+	public final String SUCCESS = "SUCCESS";
+	
 	/**
 	* 멤버 목록을 반환한다.
 	* 
@@ -30,7 +33,7 @@ public class RestApiController {
 	@RequestMapping(value = "/members", method = RequestMethod.GET)
 	public @ResponseBody List<MemberModel> getMemberList() throws Exception {
 		log.debug("########### getMemberList()");
-		return null;
+		return Arrays.asList(new MemberModel(1l, "shjo72", "조수희", "shjo72@nexon.co.kr", 26), new MemberModel(2l, "daphne", "최다희", "daphne@nexon.co.kr", 20));
 	}
 	
 	/**
@@ -46,9 +49,29 @@ public class RestApiController {
 		log.debug("########### insertMember()");
 		log.debug("########### memberSaveParamModel: {}", memberSaveParamModel);
 		
-		return "SUCCESS";
+		return SUCCESS;
 	}
 	
+	/**
+	* 멤버 목록을 삭제한다.
+	* 
+	* @return
+	* @throws Exception
+	*/
+	@RequestMapping(value = "/members", method = RequestMethod.DELETE)
+	public @ResponseBody String deleteMemberList() throws Exception {
+		log.debug("########### deleteMember()");
+		
+		return SUCCESS;
+	}
+	
+	/**
+	* memberId에 해당하는 정보를 반환한다.
+	* 
+	* @param memberId
+	* @return
+	* @throws Exception
+	*/
 	@RequestMapping(value = "/members/{memberId}", method = RequestMethod.GET)
 	public @ResponseBody MemberModel getMember(
 		@PathVariable long memberId) throws Exception {
@@ -58,6 +81,33 @@ public class RestApiController {
 		MemberModel memberModel = new MemberModel(memberId, "shjo72", "조수희", "shjo72@nexon.co.kr", 26);
 		
 		return memberModel;
+	}
+	
+	/**
+	* memberId에 해당하는 정보를 수정한다.
+	* 
+	* @param memberId
+	* @return
+	* @throws Exception
+	*/
+	@RequestMapping(value = "/members/{memberId}", method = RequestMethod.PUT)
+	public @ResponseBody String updateMember(
+		@PathVariable long memberId
+		, @RequestBody @Valid MemberSaveParamModel memberSaveParamModel) throws Exception {
+		log.debug("########### updateMember()");
+		log.debug("########### memberId: {}", memberId);
+		log.debug("########### memberSaveParamModel: {}", memberSaveParamModel);
+		
+		return SUCCESS;
+	}
+	
+	@RequestMapping(value = "/members/{memberId}", method = RequestMethod.DELETE)
+	public @ResponseBody String deleteMember(
+		@PathVariable long memberId) throws Exception {
+		log.debug("########### deleteMember()");
+		log.debug("########### memberId: {}", memberId);
+		
+		return SUCCESS;
 	}
 	
 }
